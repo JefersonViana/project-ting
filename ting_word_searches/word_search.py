c655f23778a0d2525a26893315302d33235473fb
jeferson-viana <jefersonv28@gmail.com>
2023-09-29 16:11:24 -0300

    feat: Implementando a função no arquivo ting_word_searches/word_search.py (Requisito 8 concluído)

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,34 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+# from ting_file_management.file_process import process
+from ting_file_management.queue import Queue
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def exists_word_aux(word, instance: Queue):
+    data = instance.read_instance()
+    dicts = list([])
+    for file in data:
+        ocorrencias = list([])
+        for index in range(1, len(file["linhas_do_arquivo"]) + 1):
+            linha = file["linhas_do_arquivo"][index - 1]
+            if word in linha.lower():
+                ocorrencias.append({"linha": index, "conteudo": linha})
+        if len(ocorrencias) > 0:
+            dicts.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": ocorrencias
+            })
+    return dicts
+
+
+def exists_word(word, instance: Queue):
+    return exists_word_aux(word, instance)
+
+
+def search_by_word(word, instance: Queue):
+    return exists_word_aux(word, instance)
+
+
+# project = Queue()
+# print(process("statics/nome_pedro.txt", project), "\n", "\n")
+# word = exists_word("xablau", project)
+# print(word)
