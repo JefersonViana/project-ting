c6cce1b715ba171069de4c69d286a2d1e30696b3
jeferson-viana <jefersonv28@gmail.com>
2023-09-28 23:52:22 -0300

    feat: Implementando a função txt_importer no arquivo file_management.py (Requisito 2 concluído)

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.split(".")[-1] != "txt":
+        sys.stderr.write("Formato inválido")
+    try:
+        with open(path_file, mode="r") as file:
+            return [string.split("\n")[0] for string in file.readlines()]
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
